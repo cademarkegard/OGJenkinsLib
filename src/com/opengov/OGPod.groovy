3e489a31e4e56c778a1419c99ca5ff4af2a9458a
Sid Dange <siddange@SDange-0413.local>
2018-05-04 12:57:31 -0700

    added resource constraints

--- src/com/opengov/OGPod.groovy
+++ src/com/opengov/OGPod.groovy
@@ -25,8 +25,16 @@ class OGPod {
 
   static def runYAML(script, String templateLabel, String nodeName, String yaml, closure) {
   def containers = [
-    script.containerTemplate(name: 'docker', image: 'docker:17.09', command: 'cat', ttyEnabled: true, envVars: [script.envVar(key: 'DOCKER_HOST', value: 'tcp://localhost:2375')]),
-    script.containerTemplate(name: 'dind-daemon', image: 'docker:17.09-dind', ttyEnabled: true, privileged: true)
+    script.containerTemplate(name: 'docker', image: 'docker:17.09', command: 'cat', ttyEnabled: true, envVars: [script.envVar(key: 'DOCKER_HOST', value: 'tcp://localhost:2375')],
+                                  resourceRequestCpu: '750',
+                                  resourceLimitCpu: '750',
+                                  resourceRequestMemory: '0.5G',
+                                  resourceLimitMemory: '0.5G'),
+    script.containerTemplate(name: 'dind-daemon', image: 'docker:17.09-dind', ttyEnabled: true, privileged: true,
+                                  resourceRequestCpu: '750',
+                                  resourceLimitCpu: '750',
+                                  resourceRequestMemory: '3G',
+                                  resourceLimitMemory: '3G')
   ]
 
   script.podTemplate(label: templateLabel, containers: containers, volumes: [script.emptyDirVolume(mountPath: '/var/lib/docker', memory: false)]) {
